ec95ab304556d9090b418576b7918fa4a4e61aa7
Dnyanesh.Ghan <dvghan@gmail.com>
2019-03-04 15:03:50 +0530

    Added json and xml reports

--- Cucumber_Selenium/src/test/java/runner/testrunner.java
+++ Cucumber_Selenium/src/test/java/runner/testrunner.java
@@ -9,14 +9,32 @@ import org.junit.AfterClass;
 import com.cucumber.listener.ExtentCucumberFormatter;
 import com.cucumber.listener.Reporter;
 
+/*
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-features = "src/test/java/features"
-	,glue= {"seleniumgluecode"},
-	plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}, 
-	        monochrome = true
-	//plugin = { "pretty", "html:target/htmlreports" }
+		features = "src/test/java/features",
+		glue= {"seleniumgluecode"},
+		plugin = {  "pretty", "json:target/cucumber-reports/Cucumber.json",
+					"junit:target/cucumber-reports/Cucumber.xml",
+					"html:target/cucumber-reports"},
+ 		monochrome = true
+)
+
+public class testrunner {
+
+}
+*/
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/java/features",
+		glue= {"seleniumgluecode"},
+		plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"
+				, "json:target/cucumber-reports/Cucumber.json"
+				, "junit:target/cucumber-reports/Cucumber.xml"				
+		}, 
+		monochrome = true
 )
 
 public class testrunner {
@@ -25,3 +43,4 @@ public class testrunner {
         Reporter.loadXMLConfig(new File("config/report.xml"));
     }
 }
+
